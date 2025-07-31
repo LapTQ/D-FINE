1a5a13ea02af3af799f44dde45bce4d9810c189c
LapTQ <lap.tq4@gmail.com>
2025-07-31 11:50:02 +0900

    .

---
+++ scripts/create-env.sh
@@ -0,0 +1,20 @@
+# DIR_PRJ=~/laptq-nedo-fed
+# # VENV_PARENT=/data/nedo-dsoft-data-disk1/awlvn/laptq/laptq-nedo-fed
+# VENV_PARENT=~/laptq-nedo-fed
+
+# VENV_NAME=.venv
+# VENV_PATH=$VENV_PARENT/$VENV_NAME
+# [[ ! -d $VENV_PATH ]] && python3 -m venv $VENV_PATH
+
+# if [ $( realpath "$VENV_PARENT" ) != $( realpath "$DIR_PRJ" ) ]; then
+#     ln -sf $VENV_PATH $DIR_PRJ/
+# fi
+
+# source $DIR_PRJ/$VENV_NAME/bin/activate
+which python3
+
+
+git config user.name "LapTQ"
+git config user.email "lap.tq4@gmail.com"
+
+pip install -r requirements.txt
