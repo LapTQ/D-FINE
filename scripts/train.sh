d3c9d701947f147b0558489bc592a0a3f19c861a
LapTQ <lap.tq4@gmail.com>
2025-09-05 20:24:56 +0900

    .

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,17 @@
 
-DEVICE=1
+DEVICE=0
 PORT=7780
-NAME_CFG=dfine_hgnetv2_s--c1-d6.1--c2-d6.2.yml
+NAME_CFG=dfine_hgnetv2_s--20241122--phase-2--annotation-ver2.yml
 
-cd /home/lap_awlv/laptq-nedo-fed/submodules/D-FINE
-CUDA_VISIBLE_DEVICES=$DEVICE torchrun \
-    --master_port=$PORT \
-    --nproc_per_node=1 \
-    train.py \
-    -c /home/lap_awlv/laptq-nedo-fed/submodules/D-FINE/configs/dfine/$NAME_CFG \
+cd /mnt/hdd10tb/Users/laptq/laptq-prj-46/submodules/D-FINE
+# CUDA_VISIBLE_DEVICES=$DEVICE torchrun \
+#     --master_port=$PORT \
+#     --nproc_per_node=1 \
+#     train.py \
+#     -c /mnt/hdd10tb/Users/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/$NAME_CFG \
+#     --use-amp \
+#     --seed=0 \
+CUDA_VISIBLE_DEVICES=$DEVICE python3 train.py \
+    -c /mnt/hdd10tb/Users/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/$NAME_CFG \
     --use-amp \
     --seed=0 \
