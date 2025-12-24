b9cf92a14b9993f8b4b239cd171b822aafbbf4b5
LapTQ <lap.tq4@gmail.com>
2025-12-24 19:52:46 +0900

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,12 +2,33 @@
 DEVICE=1
 PORT=7787
 
+source /home/pocuser2/miniconda3/bin/activate /home/pocuser2/laptq-nedo-fed/.venv-conda
+
 cd submodules/D-FINE
-# CUDA_VISIBLE_DEVICES=$DEVICE torchrun \
-#     --master_port=$PORT \
-#     --nproc_per_node=1 \
-CUDA_VISIBLE_DEVICES=$DEVICE python3 \
-    train.py \
-    -c /home/pocuser2/laptq-nedo-fed/submodules/D-FINE/configs/dfine/dfine_hgnetv2_s--sku.yml \
-    --test-only \
-    -r /home/pocuser2/laptq-nedo-fed/runs/data--sku/dfine_hgnetv2_s/train-B-E60-LR0.0002/best_stg2.pth \
+
+
+LS_MODELS=(
+    /home/pocuser2/laptq-nedo-fed/runs/data--sku/dfine_hgnetv2_s/train-B-E60-LR0.0002/best_stg2.pth
+    /home/pocuser2/laptq-nedo-fed/runs/data--locount/dfine_hgnetv2_s/train-B-E60-LR0.0002/best_stg2.pth
+    /home/pocuser2/laptq-nedo-fed/runs/data--sku--locount/dfine_hgnetv2_s/train-B-E80-LR0.0002/best_stg2.pth
+)
+
+LS_DATA=(
+    dfine_hgnetv2_s--sku
+    dfine_hgnetv2_s--locount
+    dfine_hgnetv2_s--sku--locount
+)
+
+
+for model in ${LS_MODELS[@]}; do
+    for data in ${LS_DATA[@]}; do
+        # CUDA_VISIBLE_DEVICES=$DEVICE torchrun \
+        #     --master_port=$PORT \
+        #     --nproc_per_node=1 \
+        CUDA_VISIBLE_DEVICES=$DEVICE python3 \
+            train.py \
+            -c configs/dfine/$data.yml \
+            --test-only \
+            -r $model \
+    done
+done
