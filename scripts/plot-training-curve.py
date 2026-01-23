7b184df6145338a4bd3877c4ef1a3327284ccd7d
LapTQ <lap.tq4@gmail.com>
2026-01-23 16:43:44 +0900

    .

--- scripts/plot-training-curve.py
+++ scripts/plot-training-curve.py
@@ -55,11 +55,8 @@ def plot_metrics(log_files_with_names, output_dir):
 
 # Example usage
 log_files = [
-    ("/home/pocuser2/laptq-nedo-fed/runs/data--coco2017/dfine_hgnetv2_s/train-B-E80-LR0.0002/log.txt", "B-coco"),
-    ("/home/pocuser2/laptq-nedo-fed/runs/data--lagenda/dfine_hgnetv2_s/train-B-E80-LR0.0002/log.txt", "B-lagenda"),
-    ("/home/pocuser2/laptq-nedo-fed/runs/data--coco2017--lagenda/dfine_hgnetv2_s/train-B-E80-LR0.0002/log.txt", "C"),
-    ("/home/pocuser2/laptq-nedo-fed/runs/data--coco2017/dfine_hgnetv2_s/fedavg--coco2017--lagenda--80x1--LR0.0002/log.txt", "G-coco"),
-    ("/home/pocuser2/laptq-nedo-fed/runs/data--lagenda/dfine_hgnetv2_s/fedavg--coco2017--lagenda--80x1--LR0.0002/log.txt", "G-lagenda"),
+    ("/home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_s/train/log.txt", "D-FINEs 960"),
+    ("/home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/log.txt", "D-FINEm 960"),
 ]
-output_dir = "outputs/plot-curve-dfine"
+output_dir = "/home/laptq/laptq-prj-46/outputs/plot-curve-dfine"
 plot_metrics(log_files, output_dir)
--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,9 +1,138 @@
 cd /home/pocuser2/laptq-nedo-fed/submodules/D-FINE
 
-python python3 torch_inf.py \
-    -c /home/pocuser2/laptq-nedo-fed/submodules/D-FINE/configs/dfine/dfine_hgnetv2_s--coco2017--lagenda.yml \
-    -r /home/pocuser2/laptq-nedo-fed/runs/data--coco2017--lagenda/dfine_hgnetv2_s/train-B-E80-LR0.0002/best_stg2.pth \
-    --input /home/pocuser2/datasets/OpenImageV7-v2/yolo_format/images/val \
-    --device cuda:0 \
-    --imgsz 640 \
-    --output_dir /home/pocuser2/laptq-nedo-fed/outputs/predict
+python tools/inference/torch_inf.py \
+    -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+    -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+    --input /mnt/ssd8tb/shared_workspace/prj57/datasets/thailand/videos/IMG_0904.MOV \
+    --device cuda:2 \
+    --imgsz 960 \
+    --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input /mnt/ssd8tb/shared_workspace/prj57/datasets/thailand/videos/IMG_0905.MOV \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input /mnt/ssd8tb/shared_workspace/prj57/datasets/thailand/videos/IMG_0906.MOV \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input /mnt/ssd8tb/shared_workspace/prj57/datasets/thailand/videos/IMG_0907.MOV \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input /mnt/ssd8tb/shared_workspace/prj57/datasets/thailand/videos/IMG_0908.MOV \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms \
+
+# =====================================
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/01_250917/250917_Highway at Night.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/01_250917 \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input /mnt/ssd8tb/shared_workspace/prj57/datasets/uae/01_250917/250917_Mussafa_pointAtoB.MOV \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir /home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/01_250917 \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/01_正面(下村）/IMG_0667.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/01_正面(下村）" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/01_正面(下村）/IMG_0668.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/01_正面(下村）" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/01_正面(下村）/IMG_0669.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/01_正面(下村）" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/01_正面(下村）/IMG_0670.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/01_正面(下村）" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/01_正面(下村）/IMG_0672.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/01_正面(下村）" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/03_左側(檜山)/IMG_8342.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/03_左側(檜山)" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/03_左側(檜山)/IMG_8343.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/03_左側(檜山)" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/03_左側(檜山)/IMG_8344.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/03_左側(檜山)" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/03_左側(檜山)/IMG_8345.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919/03_左側(檜山)" \
+
+# python tools/inference/torch_inf.py \
+#     -c /home/laptq/laptq-prj-46/submodules/D-FINE/configs/dfine/dfine_hgnetv2_m--prj57-v1.yml \
+#     -r /home/laptq/laptq-prj-46/runs/prj57-v1/dfine_hgnetv2_m/train/best_stg2.pth \
+#     --input "/mnt/ssd8tb/shared_workspace/prj57/datasets/uae/02_250919/IMG_8327.MOV" \
+#     --device cuda:2 \
+#     --imgsz 960 \
+#     --output_dir "/home/laptq/laptq-prj-46/outputs/predict_results/dfinem-960-nms/uae/02_250919" \
